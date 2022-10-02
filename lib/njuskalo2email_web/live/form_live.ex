6df8143b1f2506c244f0e5da8f1358f8cc818335
Bartol Deak <b@bdeak.net>
2022-10-02 15:46:57 +0200

    implement tabs

--- lib/njuskalo2email_web/live/form_live.ex
+++ lib/njuskalo2email_web/live/form_live.ex
@@ -4,12 +4,35 @@ defmodule Njuskalo2emailWeb.FormLive do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(:query, "")}
+     |> assign(:tab, "ads")
+     |> assign(:car_model, "")
+     |> assign(:car_model_options, [])
+     |> assign(:car_sort, "")
+     |> assign(:ad_category, "")
+     |> assign(:ad_category_options, [])
+     |> assign(:ad_query, "")
+     |> assign(:ad_query_suggestions, [])
+     |> assign(:ad_sort, "")}
   end
 
-  def handle_event("update_autocomplete", %{"value" => query}, socket) do
+  def handle_event("set_tab", %{"value" => tab}, socket) do
     {:noreply,
      socket
-     |> assign(:query, query)}
+     |> assign(:tab, tab)}
+  end
+
+  def handle_event("set_ad_query", %{"value" => query}, socket) do
+    {:noreply,
+     socket
+     |> assign(:ad_query, query)}
   end
 end
+
+# tab (checkbox)
+#   cars
+#     car model (select with tree style data)
+#     sort (select)
+#   ads
+#     category (select with tree style data)
+#     query (input with suggestions)
+#     sort (select)
